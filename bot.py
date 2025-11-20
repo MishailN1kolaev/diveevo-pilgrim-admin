b5dc241754d8e2a5b3c7ac60538de35109e3bfd0
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2025-11-20 15:40:37 +0300

    m

--- bot.py
+++ bot.py
@@ -16,8 +16,8 @@ from pathlib import Path
 import database as db
 
 # Configuration
-TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11" # Placeholder
-ADMIN_ID = int(getenv("ADMIN_ID", 123456789))
+TOKEN = "8353595718:AAEN6_8rF3feUhWOzgulM2Ns_HLYI2c45bw" # Placeholder
+ADMIN_ID = int(getenv("ADMIN_ID", 627977881))
 WEB_SERVER_HOST = "0.0.0.0"
 WEB_SERVER_PORT = 8080
 BASE_URL = "http://localhost:8080"
